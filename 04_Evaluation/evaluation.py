d1e9a2a774dbff8249640422e129a74be30e3a0a
Mike Bowyer <mbowyer@umich.edum>
2019-12-03 21:53:59 -0500

    Got the evalution script running

--- 04_Evaluation/evaluation.py
+++ 04_Evaluation/evaluation.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
-# title           :reloadModel.py
-# description     :
-# author          :Michael Bowyer
-# date            :20191126
-# version         :0.0
-# usage           :
+# title           : evalution.py
+# description     : This script takes in already infered data and generates evaluation metrics
+# author          : Michael Bowyer
+# date            : 20191126
+# version         : 0.0
+# usage           : python evaluation.py --inference_data data.csv
 # notes           :
 # python_version  :Python 3.7.3
 # ==============================================================================
+import logging
+import argparse
+import os
+import pandas as pd
+
+""" Setup logging config """
+log = logging.basicConfig(level=logging.INFO, filemode='w',
+                          format='%(levelname)s:: %(message)s')  # ,filename='app.log')
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+""" Input Arguments """
+parser = argparse.ArgumentParser(
+    description='This script takes in already infered data and generates evaluation metrics for the predicted ZHVI values')
+parser.add_argument('--inference_data', '-id', type=str, required=True,
+                    help='Input csv file which contains infered and error data from some ZHVI dataset')
+
+
+if __name__ == "__main__":
+
+    """ Parse Args """
+    args = parser.parse_args()
+    logging.info('Using input inferece file to generate metrics: ' +
+                 str(args.inference_data))
 
 # """ Generate Error Metrics for overall Training data """
 # EvalCols = evaluationDf.columns.values
